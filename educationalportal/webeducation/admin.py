6c7b75a0d9472f76c523a36d295d9e52b6d23137
stackman <228bobmarley228@gmail.com>
2024-04-17 02:59:56 +0300

    added admin

--- educationalportal/webeducation/admin.py
+++ educationalportal/webeducation/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser, Subject, Course, Task
 
-# Register your models here.
+
+admin.site.register(CustomUser)
+admin.site.register(Subject)
+admin.site.register(Course)
+admin.site.register(Task)
+
